d359e0f04876abf7e5ea13092ccf523da0fac211
L1USH <61059038+L1USH@users.noreply.github.com>
2021-10-04 14:07:41 +0200

    Add files via upload

---
+++ HelloVistulaLiush/src/pl/vistula/helloworldnazwisko/HelloWorldLiush.java
@@ -0,0 +1,11 @@
+package pl.vistula.helloworldnazwisko;
+
+/**
+ * Created by student on 10/4/2021.
+ */
+public class HelloWorldLiush {
+    public static  void  main(String[] args ){
+        System.out.println("Hello Vistula Liush");
+        System.out.println("lalalalalla");
+    }
+}
